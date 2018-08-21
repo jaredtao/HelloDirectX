adb3df7ca984acce4613a0d182da5b042f83fbdf
jared <jared2020@163.com>
2018-08-22 07:08:20 +0800

    update Quaternion

--- TaoMath/Quaternion.cpp
+++ TaoMath/Quaternion.cpp
@@ -64,10 +64,11 @@ void Quaternion::getAxisAndAngle(real& x, real& y, real& z, real& angle) const
 Vector3D Quaternion::toEulerAngles() const
 {
     real pitch, yaw, roll;
-    getEulerAngles (pitch, yaw, roll);
+    getEulerAngles(pitch, yaw, roll);
     return Vector3D(pitch, yaw, roll);
 }
-void Quaternion::getEulerAngles(real& pitch, real& yaw, real& roll) const {
+void Quaternion::getEulerAngles(real& pitch, real& yaw, real& roll) const
+{
     real xx = mX * mX;
     real xy = mX * mY;
     real xz = mX * mZ;
@@ -79,7 +80,7 @@ void Quaternion::getEulerAngles(real& pitch, real& yaw, real& roll) const {
     real zw = mZ * mScalar;
 
     const real lengthSquared = xx + yy + zz + mScalar * mScalar;
-    if (!isZero (lengthSquared - 1.0f) && !isZero (lengthSquared)) {
+    if (!isZero(lengthSquared - 1.0f) && !isZero(lengthSquared)) {
         xx /= lengthSquared;
         xy /= lengthSquared;
         xz /= lengthSquared;
@@ -90,22 +91,22 @@ void Quaternion::getEulerAngles(real& pitch, real& yaw, real& roll) const {
         zz /= lengthSquared;
         zw /= lengthSquared;
     }
-    pitch = std::asin (-2.0f * (yz - xw));
+    pitch = std::asin(-2.0f * (yz - xw));
     if (pitch < PI_2) {
         if (pitch > -PI_2) {
-            yaw = std::atan2 (2.0f * (xz + yw), 1.0f - 2.0f * (xx + yy));
-            roll = std::atan2 (2.0f * (xy + zw), 1.0f - 2.0f * (xx + zz));
+            yaw = std::atan2(2.0f * (xz + yw), 1.0f - 2.0f * (xx + yy));
+            roll = std::atan2(2.0f * (xy + zw), 1.0f - 2.0f * (xx + zz));
         } else {
             roll = 0.0f;
-            yaw = -std::atan2 (-2.0f * (xy - zw), 1.0 - 2.0f * (yy + zz));
+            yaw = -std::atan2(-2.0f * (xy - zw), 1.0 - 2.0f * (yy + zz));
         }
     } else {
         roll = 0.0f;
-        yaw = std::atan2 (-2.0f * (xy - zw), 1.0f - 2.0f * (yy + zz));
+        yaw = std::atan2(-2.0f * (xy - zw), 1.0f - 2.0f * (yy + zz));
     }
-    pitch = radiansToDegrees (pitch);
-    yaw = radiansToDegrees (yaw);
-    roll = radiansToDegrees (roll);
+    pitch = radiansToDegrees(pitch);
+    yaw = radiansToDegrees(yaw);
+    roll = radiansToDegrees(roll);
 }
 
 bool Quaternion::isNull() const
@@ -188,15 +189,15 @@ Quaternion Quaternion::fromAxisAndAngle(const Vector3D& axis, real angle)
 }
 Quaternion Quaternion::fromEulerAngles(real pitch, real yaw, real roll)
 {
-    pitch = degreesToRadians (pitch) * 0.5f;
-    yaw = degreesToRadians (yaw)* 0.5f;
-    roll = degreesToRadians (roll) * 0.5f;
-    real c1 = std::cos (yaw);
-    real s1 = std::sin (yaw);
-    real c2 = std::cos (roll);
-    real s2 = std::sin (roll);
-    real c3 = std::cos (pitch);
-    real s3 = std::sin (pitch);
+    pitch = degreesToRadians(pitch) * 0.5f;
+    yaw = degreesToRadians(yaw) * 0.5f;
+    roll = degreesToRadians(roll) * 0.5f;
+    real c1 = std::cos(yaw);
+    real s1 = std::sin(yaw);
+    real c2 = std::cos(roll);
+    real s2 = std::sin(roll);
+    real c3 = std::cos(pitch);
+    real s3 = std::sin(pitch);
     real c1c2 = c1 * c2;
     real s1s2 = s1 * s2;
     real w = c1c2 * c3 + s1s2 * s3;
@@ -207,31 +208,88 @@ Quaternion Quaternion::fromEulerAngles(real pitch, real yaw, real roll)
 }
 Quaternion Quaternion::fromEulerAngles(const Vector3D& eulerAngles)
 {
-    return fromEulerAngles (eulerAngles.x (), eulerAngles.y (), eulerAngles.z ());
+    return fromEulerAngles(eulerAngles.x(), eulerAngles.y(), eulerAngles.z());
 }
 Quaternion Quaternion::fromAxes(const Vector3D& xAxis, const Vector3D& yAxis, const Vector3D& zAxis)
 {
     return Quaternion();
 }
-void Quaternion::getAxes (Vector3D& xAxis, Vector3D& yAxis, Vector3D& zAxis) const {
-
-
+void Quaternion::getAxes(Vector3D& xAxis, Vector3D& yAxis, Vector3D& zAxis) const
+{
 }
 Quaternion Quaternion::fromDirection(const Vector3D& direction, const Vector3D& up)
 {
-    return Quaternion();
+    if (direction.isNull()) {
+        return {};
+    }
+    auto z = direction.normalized();
+    auto x = Vector3D::crossProduct(up, z);
+    if (isZero(x.lengthSquared())) {
+        return Quaternion::rotationTo(Vector3D(0.0f, 0.0f, 1.0f), z);
+    }
+    x.normalize();
+    auto y = Vector3D::crossProduct(z, x);
+    return Quaternion::fromAxes(x, y, z);
 }
 Quaternion Quaternion::rotationTo(const Vector3D& from, const Vector3D& to)
 {
-    return Quaternion();
+    auto v0 = from.normalized();
+    auto v1 = to.normalized();
+
+    real d = Vector3D::dotProduct(v0, v1) + 1.0f;
+
+    if (isZero(d)) {
+        auto axis = Vector3D::crossProduct(Vector3D(1.0f, 0.0f, 0.0f), v0);
+        if (isZero(axis.lengthSquared())) {
+            axis = Vector3D::crossProduct(Vector3D(0.0f, 1.0f, 0.0f), v0);
+        }
+        axis.normalize();
+        return { 0.0f, axis.x(), axis.y(), axis.z() };
+    }
+    d = std::sqrt(2.0f * d);
+    auto axis = Vector3D::crossProduct(v0, v1) / d;
+    return Quaternion(d * 0.5f, axis).normalized();
 }
 Quaternion Quaternion::slerp(const Quaternion& q1, const Quaternion& q2, real t)
 {
-    return Quaternion();
+    if (t <= 0.0f) {
+        return q1;
+    } else if (t >= 1.0f) {
+        return q2;
+    }
+    Quaternion q2b(q2);
+    real dot = Quaternion::dotProduct(q1, q2);
+    if (dot < 0.0f) {
+        q2b = -q2b;
+        dot = -dot;
+    }
+
+    real factor1 = 1.0f - t;
+    real factor2 = t;
+    if (!isZero(dot - 1.0f)) {
+        real angle = std::acos(dot);
+        real sinOfAngle = std::sin(angle);
+        if (sinOfAngle > 0.0000001) {
+            factor1 = std::sin((1.0f - t) * angle) / sinOfAngle;
+            factor2 = std::sin(t * angle) / sinOfAngle;
+        }
+    }
+
+    return q1 * factor1 + q2 * factor2;
 }
 Quaternion Quaternion::nlerp(const Quaternion& q1, const Quaternion& q2, real t)
 {
-    return Quaternion();
+    if (t <= 0.0f) {
+        return q1;
+    } else if (t >= 1.0f) {
+        return q2;
+    }
+    Quaternion q2b(q2);
+    real dot = Quaternion::dotProduct(q1, q2);
+    if (dot < 0.0f) {
+        q2b = -q2b;
+    }
+    return (q1 * (1.0f - t) + q2b * t).normalized();
 }
 real Quaternion::dotProduct(const Quaternion& q1, const Quaternion& q2)
 {
