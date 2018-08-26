58c62a9b74e58c1ad916974d547dc7529ea01a81
jared <jared2020@163.com>
2018-08-26 10:21:51 +0800

    Matrix4x4 remove inline

--- TaoMath/Matrix4x4.cpp
+++ TaoMath/Matrix4x4.cpp
@@ -1,32 +1,34 @@
 ﻿#include "Matrix4x4.hpp"
 
 namespace TaoMath {
-inline const Vector4D Matrix4x4::row(int r) const
+const Vector4D Matrix4x4::row(int r) const
 {
     assert(0 <= r && r < W);
     return Vector4D(m[r][0], m[r][1], m[r][2], m[r][3]);
 }
 
-void Matrix4x4::setRow (int r, const Vector4D & v) { 
-    assert (0 <= r && r < W);
-    m[r][0] = v.x ();
-    m[r][1] = v.y ();
-    m[r][2] = v.z ();
-    m[r][3] = v.w ();
+void Matrix4x4::setRow(int r, const Vector4D& v)
+{
+    assert(0 <= r && r < W);
+    m[r][0] = v.x();
+    m[r][1] = v.y();
+    m[r][2] = v.z();
+    m[r][3] = v.w();
 }
 
-inline const Vector4D Matrix4x4::column(int c) const
+const Vector4D Matrix4x4::column(int c) const
 {
     assert(0 <= c && c < H);
     return Vector4D(m[0][c], m[1][c], m[2][c], m[3][c]);
 }
 
-void Matrix4x4::setColumn (int c, const Vector4D & v) { 
-    assert (0 <= c && c < H);
-    m[0][c] = v.x ();
-    m[1][c] = v.y ();
-    m[2][c] = v.z ();
-    m[3][c] = v.w ();
+void Matrix4x4::setColumn(int c, const Vector4D& v)
+{
+    assert(0 <= c && c < H);
+    m[0][c] = v.x();
+    m[1][c] = v.y();
+    m[2][c] = v.z();
+    m[3][c] = v.w();
 }
 
 Matrix4x4& Matrix4x4::operator*=(const Matrix4x4& o)
@@ -39,8 +41,4 @@ Matrix4x4& Matrix4x4::operator*=(const Matrix4x4& o)
     }
     return *this;
 }
-
-
-
-
 }
