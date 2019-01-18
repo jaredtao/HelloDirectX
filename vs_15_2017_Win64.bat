b5233dfdba8aa0b4066ceed0ead403a6a758ba4f
jared <jared2020@163.com>
2019-01-18 09:51:38 +0800

    update vs script

--- vs_15_2017_Win64.bat
+++ vs_15_2017_Win64.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE=E:/Tools/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE="%VCPKG_PATH%scripts\buildsystems\vcpkg.cmake"
