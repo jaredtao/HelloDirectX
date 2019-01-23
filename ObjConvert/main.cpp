c48a0d853f23d09eca3a6676150b7b168728e045
jared <jared2020@163.com>
2019-01-23 23:05:16 +0800

    memory check [skip ci]

--- ObjConvert/main.cpp
+++ ObjConvert/main.cpp
@@ -23,6 +23,7 @@ void usage(char *exeName)
 }
 int main(int argc, char **argv)
 {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     if (argc != 3)
     {
         usage(argv[0]);
