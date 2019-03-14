84a17ab44b319134aeabcf8824683192b020cf28
jared <jared2020@163.com>
2019-03-14 21:23:28 +0800

    去掉DirrctXSDK

--- vs_15_2017_Win64-nmake.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mkdir build-ms
-cd build-ms
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-cmake .. -G "NMake Makefiles" -DCMAKE_TOOLCHAIN_FILE="%VCPKG_PATH%scripts\buildsystems\vcpkg.cmake"
-pause
