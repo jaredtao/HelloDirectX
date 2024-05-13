7e191e0c9853c1d76363901d5ffb21a7502505e6
jared <jared2020@163.com>
2024-05-13 21:31:08 +0800

    update

--- Tao3DCore/DXResources.cpp
+++ Tao3DCore/DXResources.cpp
@@ -420,7 +420,7 @@ void DXResources::initBlend()
 {
 	D3D11_BLEND_DESC blendDesc						= { 0 };
 	blendDesc.RenderTarget[0].BlendEnable			= true;
-	blendDesc.RenderTarget[0].SrcBlend				= D3D11_BLEND_ONE;
+	blendDesc.RenderTarget[0].SrcBlend				= D3D11_BLEND_SRC_ALPHA;
 	blendDesc.RenderTarget[0].DestBlend				= D3D11_BLEND_INV_SRC_ALPHA;
 	blendDesc.RenderTarget[0].BlendOp				= D3D11_BLEND_OP_ADD;
 	blendDesc.RenderTarget[0].SrcBlendAlpha			= D3D11_BLEND_ONE;
